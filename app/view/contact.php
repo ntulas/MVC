6998a4f34bb353ae240b48516662187a8fcd245b
ntulas <naycer.tulas@gmail.com>
2017-05-18 02:54:05 +0800

    Initial files

---
+++ app/view/contact.php
@@ -0,0 +1,15 @@
+<div id="content">
+  <div class="row">
+    <h1>Contact Us</h1>
+       <form action="sendContactForm" method="post"  class="sends-email ctc-form" >
+         <input type="text" name="name" placeholder="Name:">
+         <input type="text" name="address" placeholder="Address:">
+         <input type="text" name="email" placeholder="Email:">
+         <input type="text" name="phone" placeholder="Phone:">
+         <input type="text" name="fax" placeholder="Fax:">
+         <textarea name="message" id="" cols="30" rows="10" placeholder="Message:"></textarea>
+          <div id="recaptcha1" class="recap"></div><br>
+          <button type="submit" class="btn">Submit</button>
+      </form>
+  </div>
+</div>
