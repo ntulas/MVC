cd92070feefc16e2fd78c7bcaa7b52fd18380d7c
ntulas <naycer.tulas@gmail.com>
2017-05-25 02:15:19 +0800

    Config fix
    Removed trailing slash in URL when site is live

--- config/config.php
+++ config/config.php
@@ -35,7 +35,12 @@
 	define('URL_PROTOCOL', 'http://');
 	define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
 	define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
-	define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER . '/');
+	if(substr(URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER, -1) == '/') {
+            define ('RAW_URL',substr(URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER, 0, -1));
+      } else{
+            define ('RAW_URL',URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
+      }
+      define('URL', RAW_URL . '/');
 
 	require MVC . 'controller/controller.php';
 	require MVC . 'app.php';
